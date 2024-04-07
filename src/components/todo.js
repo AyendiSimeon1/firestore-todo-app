c8705431528ebc95bbdbe78449049c4c05d8630d
ayendisimeon3 <28042463-ayendisimeon3@users.noreply.replit.com>
2024-04-07 12:03:37 +0000

    update

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,22 +1,18 @@
-import { List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
 import { db } from "../firebase.js";
 import { doc, deleteDoc } from "firebase/firestore";
 const Todo = ({ arr }) => {
   return (
-    <List className="todo__list">
-      <ListItem>
-        <ListItemAvatar />
-        <ListItemText primary={arr.item.todo} secondary={arr.item.todo} />
-      </ListItem>
-      <DeleteIcon
+    <ul>
+        <h1 primary={arr.item.todo} secondary={arr.item.todo} />
+    
+      <li
         fontSize="large"
         style={{ opacity: 0.7 }}
         onClick={() => {
           deleteDoc(doc(db, "todos", arr.id));
         }}
       />
-    </List>
+    </ul>
   );
 };
 export default Todo;
--- src/components/todolist.js
+++ src/components/todolist.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { TextField, Button } from '@mui/material';
 import Todo from './components/Todo';
 import { db } from './firebase.js';
 import { collection, query, orderBy, onSnapshot, addDoc, serverTimestamp } from 'firebase/firestore';
@@ -28,9 +27,9 @@ function App() {
         <div className="App">
             <h2> TODO List App</h2>
             <form>
-                <TextField id="outlined-basic" label="Make Todo" variant="outlined" style={{ margin: "0px 5px" }} size="small" value={input}
+                <input  label="Make Todo"  style={{ margin: "0px 5px" }} size="small" value={input}
                     onChange={e => setInput(e.target.value)} />
-                <Button variant="contained" color="primary" onClick={addTodo}  >Add Todo</Button>
+                <Button color="primary" onClick={addTodo}  >Add Todo</Button>
             </form>
             <ul>
                 {todos.map(item => <Todo key={item.id} arr={item} />)}
